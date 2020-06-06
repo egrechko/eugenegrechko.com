a185b09e96b0065c406df2cf30882c83f9d0a667
Eugene Grechko <eugene.grechko@gmail.com>
2020-06-06 11:38:13 -0700

    Added a hero to the home page. Updated seo on home page.

---
+++ components/HomeHero.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hero is-light is-medium">
+    <div class="hero-body">
+      <div class="container">
+        <h2 class="title has-text-dark">
+          Eugene Grechko Freelance Web Developer in Portland Oregon
+        </h2>
+        <hr />
+        <p>
+          My name is Eugene Grechko. I am a web developer & entrepreneur who
+          likes to tinker around with technology. I built this site to document
+          the things I am learning about and help others with the same issues
+          I'm having.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+h2 {
+  font-size: 3.6rem;
+}
+p {
+  color: $light;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,27 @@
 <template>
-  <div class="section">
-    <div class="container">
-      <h1 class="title is-size-2">Latest Blog Posts</h1>
-      <BlogPostCard
-        class="blog-posts"
-        v-for="post in posts"
-        :key="post.title"
-        :post="post"
-      />
+  <div>
+    <HomeHero />
+    <div class="section">
+      <div class="container">
+        <h1 class="title is-size-2">Latest Blog Posts</h1>
+        <BlogPostCard
+          class="blog-posts"
+          v-for="post in posts"
+          :key="post.title"
+          :post="post"
+        />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import HomeHero from '~/components/HomeHero'
 import BlogPostCard from '~/components/BlogPostCard'
 
 export default {
   components: {
+    HomeHero,
     BlogPostCard
   },
   data() {
@@ -30,6 +35,41 @@ export default {
       .sortBy('date', 'desc')
       .limit(5)
       .fetch()
+  },
+  head() {
+    const seoTitle = 'Eugene Grechko Freelance Web Developer Portland Oregon'
+
+    const seoDescription =
+      "My name is Eugene Grechko. I am a web developer & entrepreneur who likes to tinker around with technology. I built this site to document the things I am learning about and help others with the same issues I'm having."
+
+    return {
+      title: seoTitle,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: seoDescription
+        },
+        // Open Graph
+        { hid: 'og:title', property: 'og:title', content: seoTitle },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: seoDescription
+        },
+        // Twitter Card
+        {
+          hid: 'twitter:title',
+          name: 'twitter:title',
+          content: seoTitle
+        },
+        {
+          hid: 'twitter:description',
+          name: 'twitter:description',
+          content: seoDescription
+        }
+      ]
+    }
   }
 }
 </script>
