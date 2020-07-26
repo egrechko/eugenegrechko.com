9871da79619f87c13aa561584aedc3170dff124e
Eugene Grechko <eugene.grechko@gmail.com>
2020-07-25 18:14:15 -0700

    Added blog post route generation to nuxt.config.js.

--- nuxt.config.js
+++ nuxt.config.js
@@ -70,6 +70,17 @@ export default {
    ** See https://axios.nuxtjs.org/options
    */
   axios: {},
+  // generate routes for blog posts
+  generate: {
+    async routes() {
+      const { $content } = require('@nuxt/content')
+      const files = await $content('blog')
+        .only(['path'])
+        .fetch()
+
+      return files.map((file) => (file.path === '/index' ? '/' : file.path))
+    }
+  },
   /*
    ** Build configuration
    */
